ec817f36c6e32f585534907ee9fb4623c7154f96
aliakbarbadri <aabadri74@gmail.com>
2022-09-12 18:14:10 +0430

    add signin endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,13 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,10 +19,14 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.usersRepository.create({
       username,
-      password,
+      password: hashedPassword,
     });
+
     try {
       await this.usersRepository.save(user);
     } catch (error) {
@@ -29,6 +35,17 @@ export class AuthService {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'Success';
+    } else {
+      throw new UnauthorizedException('Wrong!Check your username and password');
     }
   }
 }
--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,13 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+// import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
-import {
-  Injectable,
-  InternalServerErrorException,
-  Logger,
-} from '@nestjs/common';
+// import {
+//   Injectable,
+//   InternalServerErrorException,
+//   Logger,
+// } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
