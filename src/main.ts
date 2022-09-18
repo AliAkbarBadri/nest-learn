16d269937c2416bccf1fac348c2a027dfc4e3fdc
aliakbarbadri <aabadri74@gmail.com>
2022-09-18 09:55:06 +0430

    add var to env variable with 'MY_VAR=FOLAN npm run start:dev'

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
+console.log(process.env.MY_VAR);
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalInterceptors(new TransformInterceptor());
+  // await app.listen(3000);
 }
 bootstrap();
